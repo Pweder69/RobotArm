95b0fa89dece1134758ff19585fbe0529f9f1ef8
pweder69 <pweder07@charlottesvilleschools.org>
2023-01-11 13:15:13 -0500

    basic structure

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello world")
+import time
+import math
+import digitalio
+import adafruit_motor
+import board
+import pwmio
+from analogio import AnalogIn, AnalogOut 
+
+coils =[
+    digitalio.DigitalInOut(board.D8),  # A1
+    digitalio.DigitalInOut(board.D9),  # A2
+    digitalio.DigitalInOut(board.D10),  # B1
+    digitalio.DigitalInOut(board.D11),  # B2
+    ]
+
+pwm = pwmio.PWMOut(board.D7, duty_cycle=2 ** 15, frequency=50)
+motor = stepper.StepperMotor(coils[0], coils[1], coils[2], coils[3], microsteps=None)
+
+def remap(x, in_min, in_max, out_min, out_max):
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+def stepperAdditionMultiplier(xPotentiometer):
+    remap(xPotentiometer,0,65535,-10,10)
+    return xPotentiometer
+
+while True():
+    print(stepperAdditionMultiplier(AnalogIn(board.A0)))
+
